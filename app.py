ed2c2b3e79ffe1cbc1f89b8c47374f2b86048747
Augusto Pinto Meine <50183304+AugustoMeine@users.noreply.github.com>
2022-06-24 19:41:43 -0300

    .

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st 
 
-st.write("Olá mundo")
+arvore = pickle.load(open("arvoreSalva.sav",'rb'))
+
+p1 = st.selectbox(
+     'Escolha uma das opções', 
+     ('1', '2', '3'))
+
+st.write('Voce escolheu:', p1)
+
+st._input ("test",key='var')
